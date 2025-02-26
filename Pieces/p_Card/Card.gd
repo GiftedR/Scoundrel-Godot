408acc305aedc1448c9df8122d75af2b46cb62fa
GiftedR <kidsgamers@hotmail.com>
2025-02-25 22:35:43 -0800

    Finished Hover Border, Added Card Hovering

--- Pieces/p_Card/Card.gd
+++ Pieces/p_Card/Card.gd
@@ -1,5 +1,22 @@
 extends Area2D
 class_name Card
 
+@export var rotScale:Vector2 = Vector2(2,2)
+
 static func create() -> Card:
 	return load("res://Pieces/p_Card.tscn").instantiate()
+
+func _enter_tree() -> void:
+	mouse_entered.connect(func() -> void:
+		$asp2_Card_Outline.visible = true
+	)
+	
+	mouse_exited.connect(func() -> void:
+		$asp2_Card_Outline.visible = false
+	)
+
+static var lastMessage:String = ""
+static func distinct_print(message:Array) -> void:
+	if str(message) != lastMessage:
+		print(message)
+		lastMessage = str(message)
